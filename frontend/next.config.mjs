ffa5548e249cc6879a865c8d14850e0c48641e88
TrendiKalaIt <trendikalait@gmail.com>
2025-10-17 00:28:43 +0530

    next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  reactStrictMode: true, // React ke strict mode me run kare
+  swcMinify: true,       // Faster builds aur smaller bundle size
 };
 
 export default nextConfig;
