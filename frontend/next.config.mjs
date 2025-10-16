c72cc3023022cc052a37268d2caf78445b661816
TrendiKalaIt <trendikalait@gmail.com>
2025-10-17 00:23:09 +0530

    next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,6 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // ye add karo agar aap static export karna chahte ho
+
 };
 
 export default nextConfig;
